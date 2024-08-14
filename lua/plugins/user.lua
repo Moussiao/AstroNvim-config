ce87cf0d48029ec4bb9e8458d89f18fbdf29ddd7
Илья Нестеренко <ilya-nest@yandex.ru>
2024-08-14 16:43:30 +0200

    first commit

---
+++ lua/plugins/user.lua
@@ -0,0 +1,28 @@
+-- == User settings ==
+
+-- == Lenght line for *.py files == 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "python",
+  callback = function()
+    vim.api.nvim_set_option_value("colorcolumn", "88", {})
+  end
+})
+
+
+return {
+  {
+    -- == Auto save == -- 
+    
+    "okuuva/auto-save.nvim",
+    cmd = "ASToggle", -- optional for lazy loading on command
+    event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+    opts = { 
+      enabled = true, -- start auto-save when the plugin is loaded
+      debounce_delay = 1500, -- delay after which a pending save is executed
+    },
+    keys = {
+      { "<leader>n", ":ASToggle<CR>", desc = "Toggle auto-save" },
+    }, 
+  }
+
+}
